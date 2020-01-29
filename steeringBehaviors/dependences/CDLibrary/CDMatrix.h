fac20c6f4aebd2a666a50a066ed72bd1d2ae6cc9
serbolsd <32022302+serbolsd@users.noreply.github.com>
2020-01-28 23:57:15 -0600

    Borrado de version de visual 2017
    Borro la version del 2017 para subir la version de visual del 2019

--- steeringBehaviors/dependences/CDLibrary/CDMatrix.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <CDMatrix/CDMatrix2.h>
-#include <CDMatrix/CDMatrix3.h>
-#include <CDMatrix/CDMatrix4.h>
--- steeringBehaviors/steeringBehaviors/dependences/CDLibrary/CDVectors.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <CDVectors/CDVector2.h>
-#include <CDVectors/CDVector3.h>
-#include <CDVectors/CDVector4.h>
